5f752cdb180be93803ebfb81e70dbbc91648e77a
harshith0712 <kolagatlaharshith@gmail.com>
2020-10-08 10:25:23 +0530

    usecase2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,45 +1,25 @@
 #!/bin/bash
 
-echo "welcome to flip coin simulator "
-function coin() {
 HEAD=0
 TAIL=1
-
-headcound=0
-tailcount=0
-	while [[ $headcount -le 21 && $tailcount -le 21 ]]
+echo "enter number"
+read number
+count=0
+	while [ $count -le $number ]
 	do
 		CoinFlip=$((RANDOM%2))
 
 			case $CoinFlip in $HEAD)
 				echo "its heads"
-				((headcount++))
+				((count++))
 					;;
 		 	$TAIL)
 				echo "its tails"
-					((tailcount++))
+					((count++))
 					;;
 				*)
 				echo "enter correct value"
 					;;
 			esac
 	done
-}
-coin;
-
-	if [ $headcount -gt $tailcount ]
-	then
-		difference=$(($headcount - $tailcount))
-		echo "heads won with a margin of $difference"
-	elif [ $tailcount -gt $headcount ]
-	then
-		difference=$(($tailcount - $headcount))
-		echo "tails won with a margin of $difference"
-	else
-		difference=$(($headcount - $tailcount))
-		while [$difference -gt 2]
-		do
-			coin;
-		done
-	fi
 
